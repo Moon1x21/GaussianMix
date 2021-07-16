be2b1b0f8251094d41fb9d40e85e9b7e9e6cf9ec
Yuje Lee <dbwp031@khu.ac.kr>
2021-07-16 08:15:51 +0000

    for automatic

--- scripts/sicap.sh
+++
@@ -1,8 +0,0 @@
-apt-get update
-apt-get install -y libgl1-mesa-glx
-apt-get install -y libglib2.0-0
-
-pip install opencv-contrib-python
-
-cd /root/volume/SICAP
-python main.py --dataset cifar100 --model WideResNetDropout --depth 28 --params 10 --beta_of_ricap 0.3 --postfix ricap0.3 --stype 'mid'
