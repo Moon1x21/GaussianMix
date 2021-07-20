292a4e5cffcaa5418ce6f5ce746e737d56191438
Yuje Lee <dbwp031@khu.ac.kr>
2021-07-20 03:40:56 +0000

    modify sh files for resnet

--- code_files/1_append_origin/sicap_cifar100.sh
+++ code_files/1_append_origin/sicap_cifar100.sh
@@ -6,5 +6,5 @@ pip install opencv-contrib-python
 
 HERE=$(dirname $(realpath $0))
 cd $HERE
-python main.py --dataset cifar100 --model WideResNetDropout --depth 28\
+python main.py --dataset cifar100 --model ResNet18 --depth 28\
  --params 10 --beta_of_ricap 0.3 --stype 'mid' --postfix ricap0.3 
